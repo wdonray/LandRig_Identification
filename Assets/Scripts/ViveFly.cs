b6731f1b2b4156622b8005316773c27195754239
Eric Mouledoux <EricMouledoux@gmail.com>
2018-10-24 10:40:08 -0500

    location markers

--- Assets/Scripts/ViveFly.cs
+++ Assets/Scripts/ViveFly.cs
@@ -20,26 +20,4 @@ public class ViveFly : MonoBehaviour
             if (Player.rightController.GetHairTrigger())
                 Player.transform.Translate(Player.rightHand.gameObject.transform.forward * speed * Time.deltaTime);
     }
-
-    IEnumerator Fly()
-    {
-        takeOffPoint = transform.position;
-        inFlight = true;
-
-        while (!left.menuPressed && !right.menuPressed)
-        {
-            float tSpeed = speed * Time.deltaTime;
-
-            if (right.triggerPressed)
-                Player.transform.Translate(right.gameObject.transform.forward * tSpeed);
-
-            if (left.triggerPressed)
-                Player.transform.Translate(left.gameObject.transform.forward * tSpeed);
-
-            yield return null;
-        }
-
-        Player.transform.position = takeOffPoint;
-        inFlight = false;
-    }
 }
